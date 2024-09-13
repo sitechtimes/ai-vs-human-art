4e2d2bcd20c49007a950bc7a406198aca31f0255
Richard M. <richardm246@nycstudents.net>
2024-09-13 10:41:35 -0400

    Revert "NPM i"
    This reverts commit 4d1ddd0a1f79eb73b96c8c34ff7257cf71beae12.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello MEVN s Stack!");
-});
-// Serve Vue app
-app.use(express.static(path.join(__dirname, "vue-app/dist")));
-
-// Handle all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "vue-app/dist/index.html"));
+  res.send("Hello MEVN Stack!");
 });
 
 app.listen(port, () => {
